ed85c247d9743fa6c80b59695a035435e229a61b
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 00:03:33 +0530

    addded neccesary imports

--- src/multithreadedserver/Main/ClientRegistry.java
+++ src/multithreadedserver/Main/ClientRegistry.java
@@ -1,4 +1,10 @@
 package multithreadedserver.Main;
 
+import multithreadedserver.readersandwriters.SocketWriter;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
 public class ClientRegistry {
 }
