54f955082f9d7dd6f5e51177391d2a103ad7117e
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:03:40 +0530

    added socket reader which abstracts buffered readers

--- src/multithreadedserver/readersandwriters/SocketReader.java
+++ src/multithreadedserver/readersandwriters/SocketReader.java
@@ -6,4 +6,22 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class SocketReader {
+
+    private final BufferedReader reader;
+
+    public SocketReader(Socket socket) throws IOException {
+        this.reader =
+                new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+    }
+
+    public String readLine() throws IOException {
+        return reader.readLine(); // blocks
+    }
+
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException ignored) {}
+    }
 }
