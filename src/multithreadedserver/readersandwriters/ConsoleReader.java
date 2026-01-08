459fef6d7a4fdb7d8abd38196a73cbef386253d0
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:01:54 +0530

    initial commit

--- src/multithreadedserver/readersandwriters/ConsoleReader.java
+++ src/multithreadedserver/readersandwriters/ConsoleReader.java
@@ -1,4 +1,30 @@
 package multithreadedserver.readersandwriters;
 
 public class ConsoleReader {
+
+    private static final BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+
+    private ConsoleReader() {}
+
+    public static String readLine(String prompt) {
+        while(true){
+            try {
+                System.out.println(prompt);
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read from console", e);
+            }
+        }
+    }
+
+    public static String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read from console", e);
+        }
+    }
+
 }
+
