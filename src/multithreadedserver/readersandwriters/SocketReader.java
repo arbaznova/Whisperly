6777f3229a78ce849b02b69eb209ae21a13ad8b3
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:02:20 +0530

    necessary imports

--- src/multithreadedserver/readersandwriters/SocketReader.java
+++ src/multithreadedserver/readersandwriters/SocketReader.java
@@ -1,4 +1,9 @@
 package multithreadedserver.readersandwriters;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class SocketReader {
 }
