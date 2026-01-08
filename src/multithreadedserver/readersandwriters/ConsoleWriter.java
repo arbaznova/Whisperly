d75756bda3f5290d17c8e9fcd450de35849ab499
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:09:28 +0530

    added console writer that prints messages on console

--- src/multithreadedserver/readersandwriters/ConsoleWriter.java
+++ src/multithreadedserver/readersandwriters/ConsoleWriter.java
@@ -1,4 +1,11 @@
 package multithreadedserver.readersandwriters;
 
 public class ConsoleWriter {
-}
+    private ConsoleWriter() {}
+    
+    public static void println(String message) {
+         System.out.println(message);
+        }
+    }
+
+
