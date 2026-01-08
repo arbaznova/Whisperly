3b619096d7916eeb9950ed2e11a6a6fd411bd57b
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:04:34 +0530

    added important imports

--- src/multithreadedserver/readersandwriters/SocketWriter.java
+++ src/multithreadedserver/readersandwriters/SocketWriter.java
@@ -1,4 +1,9 @@
 package multithreadedserver.readersandwriters;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class SocketWriter {
 }
