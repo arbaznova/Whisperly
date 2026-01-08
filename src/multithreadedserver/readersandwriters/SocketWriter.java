07cdce5093b47940508d79e070d5a75f986cfaaa
Arbaz <mohdarbazkhilji@gmail.com>
2026-01-08 16:05:04 +0530

    intial commit

--- src/multithreadedserver/readersandwriters/SocketWriter.java
+++ src/multithreadedserver/readersandwriters/SocketWriter.java
@@ -6,4 +6,17 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class SocketWriter {
-}
+
+    private final BufferedWriter writer;
+
+    public SocketWriter(Socket socket) throws IOException {
+        this.writer =
+                new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    public synchronized void write(String message) throws IOException {
+        writer.write(message);
+        writer.newLine();
+        writer.flush();
+    }
